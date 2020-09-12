b4c81a7f1d5f2c67a4d5dbd3d68642de9c143483
Cristian Reyes <creybo@gmail.com>
2020-09-11 23:54:21 -0500

    Login normal seccion 10

--- server/config/config.js
+++ server/config/config.js
@@ -8,6 +8,20 @@ process.env.PORT = process.env.PORT || 3000;
 // ===========================
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
+// =============================================
+// V e n c i m i e n t o    d e l    T o k e n
+// =============================================
+// 60 segundos
+// 60 minutos
+// 24 horas
+// 30 días
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+
+// =============================================
+// S E E D   d e   a u t e n t i c a c i ó n
+// =============================================
+process.env.SEED = process.env.SEED || 'este-es-el seed-desarrollo';
+
 // ===========================
 //  B A S E   D E   D A T O S
 // ===========================
